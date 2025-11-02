99a20a70cca1c1e1e500e076f5d14ac7da1c86af
Matthew Coulter <53892067+mattcoulter7@users.noreply.github.com>
2025-11-02 18:12:31 +1100

    feat: add database http error handling

--- src/ab_service/template/main.py
+++ src/ab_service/template/main.py
@@ -7,6 +7,7 @@ from ab_core.alembic_auto_migrate.service import AlembicAutoMigrate
 from ab_core.database.databases import Database
 from ab_core.dependency import Depends, inject
 from ab_core.logging.config import LoggingConfig
+from ab_core.sqlalchemy_fastapi_http_exceptions import register_database_exception_handlers
 from fastapi import FastAPI
 
 from ab_service.template.routes.heartbeat import router as heartbeat_router
@@ -27,4 +28,5 @@ async def lifespan(
 
 
 app = FastAPI(lifespan=lifespan)
+register_database_exception_handlers(app)
 app.include_router(heartbeat_router)
